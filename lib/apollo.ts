7491b06e3474e3b1a0f8c15faad7ce09bfb5f2cf
ajiYudhanto <aji.yudhanto@gmail.com>
2020-11-27 00:42:13 +0700

    done: - removing localhost:PORT in uri lib/apollo.ts

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -4,7 +4,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 const apolloClient = new ApolloClient({
   ssrMode: typeof window === "undefined",
   link: new HttpLink({
-    uri: "http://localhost:8080/api/graphql", // Server URL (must be absolute)
+    uri: "/api/graphql", // Server URL (must be absolute)
     credentials: "same-origin" // Additional fetch() options like `credentials` or `headers`
   }),
   cache: new InMemoryCache()
